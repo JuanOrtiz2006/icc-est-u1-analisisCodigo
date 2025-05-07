aa5b4ebd5e691153ac9c3da26b92bd832c219396
JuanOrtiz2006 <150272778+JuanOrtiz2006@users.noreply.github.com>
2025-05-07 08:46:46 -0500

    (FEAD) - Uso de Matlib

--- src_python/app.py
+++ src_python/app.py
@@ -1,22 +1,49 @@
 from metodos_ordenamiento import MetodosOrdenamiento
 from benchmarking import Benchmarking
+import matplotlib
+import matplotlib.pyplot as plt
+
 if __name__ == "__main__":
     print("Funciona")
     marking=Benchmarking()
     metodos = MetodosOrdenamiento()
-    tam=10
-    arreglo = marking.build_arreglo(tam)
-    metodosOrdenamiento = { "Burbuja": metodos.sortByBuble,
+    tamanio=[500, 1000, 2000]
+    resultados = []
+    for tam in tamanio:
+        arreglo = marking.build_arreglo(tam)
+        metodosOrdenamiento = { "Burbuja": metodos.sortByBuble,
                             "Seleccion": metodos.sort_seleccion
                           }
     
-    resultados = []
-    for nombre, metodo in metodosOrdenamiento.items():
-        tiempo = marking.medir_tiempo(metodo,arreglo)
-        respuesta = (tam, nombre, tiempo)
-        resultados.append(respuesta)
+        for nombre, metodo in metodosOrdenamiento.items():
+            tiempo = marking.medir_tiempo(metodo,arreglo)
+            respuesta = (tam, nombre, tiempo)
+            resultados.append(respuesta)
 
-    for posicion in resultados:
-        tam, nombre, tiempo = posicion
-        print(f"Tamaño: {tam}, Metodo: {nombre}, tiempo: {tiempo:.6f} segundos")
+        for posicion in resultados:
+            tam, nombre, tiempo = posicion
+            print(f"Tamaño: {tam}, Metodo: {nombre}, tiempo: {tiempo:.6f} segundos")
 
+        tiempos_by_metodo = {
+            "Burbuja": [],
+            "Seleccion": []
+        }
+
+        for tam,nombre,tiempo in resultados:
+            tiempos_by_metodo[nombre].append(tiempo)
+        print(tiempos_by_metodo)
+    
+    # graficar una linea de timepo para cada metodo
+    # x sean los tamanios
+    # y sean los tiempos
+
+    plt.figure(figsize=(10, 6))
+    for nombre, tiempos in tiempos_by_metodo.items():
+        plt.plot(tamanio, tiempos, label=nombre, marker="o")
+
+    plt.grid()
+    plt.title("Tiempos de Ordenamiento")
+    plt.xlabel("Tamaño del arreglo")
+    plt.ylabel("Tiempo (segundos)")
+    plt.legend()
+    plt.show()
